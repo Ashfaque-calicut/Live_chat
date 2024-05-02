cad88049478dacdc49742e3589085643ecdc176a
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2024-05-02 21:19:28 +0530

    sidebar rough design complete

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./myStyles.css";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { IconButton } from "@mui/material";
@@ -6,8 +6,26 @@ import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import NightlightIcon from "@mui/icons-material/Nightlight";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import ConversationItem from "./ConversationItem";
 function Sidebar() {
+  const [conversation, setConversation] = useState([
+    {
+      name: "Test#1",
+      lastMessage: "Last Message #1",
+      timeStamp: "today",
+    },
+    {
+      name: "Test#2",
+      lastMessage: "Last Message #2",
+      timeStamp: "today",
+    },
+    {
+      name: "Test#3",
+      lastMessage: "Last Message #3",
+      timeStamp: "today",
+    },
+  ]);
   return (
     <div className="sidebar-container">
       <div className="sb-header">
@@ -33,13 +51,18 @@ function Sidebar() {
       </div>
       <div className="sb-search">
         <IconButton>
-        <SearchIcon/>
+          <SearchIcon />
         </IconButton>
-       
-        <input placeholder="search" className="search-box"/>
+
+        <input placeholder="search" className="search-box" />
       </div>
 
-      <div className="sb-conversation">conversations</div>
+      <div className="sb-conversation">
+        {conversation.map((conversation)=>{
+         return <ConversationItem props={conversation}/>
+
+        })}
+      </div>
     </div>
   );
 }
