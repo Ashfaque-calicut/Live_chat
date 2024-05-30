4ae271b8b7544b0639029f14f08f6aeac4cd1801
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2024-05-30 19:52:16 +0530

    Added style

--- client/src/Components/ChatArea.js
+++ client/src/Components/ChatArea.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function ChatArea() {
   return (
-    <div className='chatArea-container'>ChatArea</div>
+    <div className='chatArea-container'>
+        <div className='chatArea-header'>ChatArea-Header</div>
+        <div className='message-container'>Message Container</div>
+        <div className='text-input-area'>text-input-area</div>
+    </div>
   )
 }
 
